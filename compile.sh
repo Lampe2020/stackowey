cb6519b40dd1489e49d3adf452b0f9cc6cd1d828
Lampe2020 <kontakt@lampe2020.de>
2025-04-05 21:12:14 +0200

    Ensure use of C++23 standard

--- compile.sh
+++ compile.sh
@@ -6,14 +6,14 @@ then
     rm -v stackowey.exe
     printf "Compiling...\n"
     #TODO: Reencode source to windows-1258 encoding first? 
-    wine C:\\Dev-Cpp\\bin\\g++.exe -finput-charset=UTF-8 -fexec-charset=UTF-8 -pipe -o stackowey.exe --debug main.cpp &&
+    wine C:\\Dev-Cpp\\bin\\g++.exe -std=c++23 -finput-charset=UTF-8 -fexec-charset=UTF-8 -pipe -o stackowey.exe --debug main.cpp &&
     printf "Running...\n" &&
     wineconsole stackowey.exe
     printf 'Ended with exit code %s\n' "$?"
 else
     rm -v stackowey
     printf "Compiling...\n"
-    g++ -o stackowey --debug -pipe main.cpp &&
+    g++ -std=c++23 -o stackowey --debug -pipe main.cpp &&
     printf "Running...\n" &&
     ./stackowey
     printf 'Ended with exit code %s\n' "$?"
