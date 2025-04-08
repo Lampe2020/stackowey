a047a788f871aec6bb6d0944ecc0014c0676db5a
Lampe2020 <kontakt@lampe2020.de>
2025-04-08 21:52:07 +0200

    Enable debug in test run after build

--- compile.sh
+++ compile.sh
@@ -8,13 +8,13 @@ then
     #TODO: Reencode source to windows-1258 encoding first? 
     wine C:\\Dev-Cpp\\bin\\g++.exe -std=c++23 -finput-charset=UTF-8 -fexec-charset=UTF-8 -pipe -o stackowey.exe --debug main.cpp &&
     printf "Running...\n" &&
-    wineconsole stackowey.exe
+    wineconsole stackowey.exe -d
     printf 'Ended with exit code %s\n' "$?"
 else
     rm -v stackowey
     printf "Compiling...\n"
     g++ -std=c++23 -o stackowey --debug -pipe main.cpp &&
     printf "Running...\n" &&
-    ./stackowey
+    ./stackowey -d
     printf 'Ended with exit code %s\n' "$?"
 fi
