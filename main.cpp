044becfe59d841119cbabf81ed061c4031a39473
Lampe2020 <kontakt@lampe2020.de>
2025-04-09 21:42:28 +0200

    Implement all commands
    → Note that whirlpool is subject to potential removal
    → Note that the name for "cross"('+') is subject to potential change
    → I'm too lazy to pluck this apart, so here's _yet another_ giant
      monolithic commit.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <locale>
 #include <map>
+#include <random>
 #include <string>
 #include <vector>
 
@@ -54,7 +55,7 @@ void cleanup() {
     std::cerr.flush();
     std::cout.flush();
     if (dev_mode) {
-        debug_info << "\n\n--- end of log ---" << std::endl;
+        debug_info << "\n--- end of log ---" << std::endl;
         debug_info.flush();
         debug_info.close();
     }
@@ -76,34 +77,96 @@ std::string getErrorName(ErrorCode err) {
 
 void error_out(ErrorCode err, std::string msg) {
     if (err == E_SUCCESS) {
-        std::cerr << getErrorName(err) << '[' << err
+        std::cerr << getErrorName(err) << '[' << std::dec << err
                   << "]: Unexpected lack of failure." << std::endl;
+        if (dev_mode)
+            debug_info << getErrorName(err) << '[' << std::dec << err
+                       << "]: Unexpected lack of failure." << std::endl;
         return; // Try to continue
-    } else if (err == E_EXIT)
+    } else if (err == E_EXIT) {
         std::exit(0);
-    else
-        std::cerr << getErrorName(err) << '[' << err << ']' << ": "
+        if (dev_mode)
+            debug_info << "Exiting by exit error with message\n"
+                       << msg << std::endl;
+    } else {
+        std::cerr << getErrorName(err) << '[' << std::dec << err << ']' << ": "
                   << ((bool)msg.size() ? msg : std::to_string(err)) << std::endl;
-    std::exit(err);
+        if (dev_mode)
+            debug_info << getErrorName(err) << '[' << std::dec << err << ']'
+                       << ": " << ((bool)msg.size() ? msg : std::to_string(err))
+                       << "\nExiting because of error." << std::endl;
+        std::exit(err);
+    }
+}
+
+std::string n_times_char(uint64_t n, char c) {
+    std::vector<char> sequence;
+    if (n < 1)
+        return std::string(""); // Cannot put less than none into a string, so just return an empty string.
+    for (int i= 0; i < n; i++) {
+        sequence.push_back(c);
+    }
+    return sequence.data();
+}
+
+uint64_t pop_stack() {
+    uint64_t value;
+    if (stack.size()) {
+        value= stack.back();
+        stack.pop_back();
+        if (dev_mode)
+            debug_info << "Popped value 0o" << std::oct << value << " off stack"
+                       << std::endl;
+    } else {
+        static std::mt19937_64 random_number_gen(std::random_device{}());
+        value= random_number_gen();
+        if (dev_mode)
+            debug_info << "Stack was empty!\n    Generated random value 0o"
+                       << std::oct << value << " instead" << std::dec << std::endl;
+    }
+    if (dev_mode)
+        debug_info << "Stack size is now 0o" << stack.size() << '.' << std::dec
+                   << std::endl;
+    return value;
+}
+
+void push_stack(uint64_t value) {
+    stack.push_back(value);
+    if (dev_mode)
+        debug_info << "Pushed value 0o" << std::oct << value << " to stack.\nStack size is now 0o"
+                   << std::oct << stack.size() << '.' << std::dec << std::endl;
 }
 
 void read_source_from_stream(std::istream& source) {
     source_from_stdin= (&source == &std::cin);
+    if (source_from_stdin && dev_mode)
+        debug_info << "Reading source from stdin..." << std::endl;
     if (source.fail())
         error_out(E_STREAM_R, "Could not read source stream!");
     std::string line;
     std::getline(source, line);
-    if (line[0] == '#' && line[1] == '!')
+    if (dev_mode)
+        debug_info << "Read first line:\n    " << line << std::endl;
+    if (line[0] == '#' && line[1] == '!') {
+        if (dev_mode)
+            debug_info << "First line was the shebang line, ignoring..." << std::endl;
         std::getline(source, line); // Ignore the shebang line if it exists
-    playfield.push_back(line);
+    }
     linelen= line.size();
-    debug_info << "Detected width " << linelen << std::endl;
-    while (std::getline(source, line) &&
-           !(source_from_stdin && line == "---")) {
+    if (dev_mode)
+        debug_info << "Detected playfield width 0o" << std::oct << linelen
+                   << std::dec << "\nStackowey source:\n    +"
+                   << n_times_char(linelen, '-') << '+' << std::endl;
+    do {
+        if (dev_mode)
+            debug_info << "    |" << line << '|' << std::endl;
         if (line.size() != linelen)
             error_out(E_SYNTAX, "The eastward edge is too rough!");
         playfield.push_back(line);
-    }
+    } while (std::getline(source, line) &&
+             !(source_from_stdin && line == "---"));
+    if (dev_mode)
+        debug_info << "    +" << n_times_char(linelen, '-') << '+' << std::endl;
 }
 
 void close_source() {
@@ -111,6 +174,9 @@ void close_source() {
 }
 
 void read_source(const char* source_path) {
+    if (dev_mode)
+        debug_info << "Reading source file from following location:\n    "
+                   << source_path << std::endl;
     std::ifstream source(source_path);
     std::atexit(close_source);
     read_source_from_stream(source);
@@ -126,16 +192,16 @@ void get_input() {
     uint64_t next_stack_value;
     std::vector<char32_t> unicode_line= unicodepoints(line);
     for (uint64_t i= 0; i < line.size(); i++) {
-        stack.push_back(uint64_t(unicode_line[i]));
+        push_stack(uint64_t(unicode_line[i]));
     }
     stack.push_back(0ull);
 }
 
 void send_output() {
+    uint64_t character= pop_stack();
     if (dev_mode)
-        debug_info << "Outputting U+" << stack.back() << std::endl;
-    std::cout << utf8_encode(stack.back() % UINT32_MAX); // Modulo to prevent invalid codepoint
-    stack.pop_back(); // Actually remove the value from the stack
+        debug_info << "Outputting U+" << std::hex << character << std::dec << std::endl;
+    std::cout << utf8_encode(character % UINT32_MAX); // Modulo to prevent invalid codepoint
 }
 
 int main(int argc, char* argv[], char* envp[]) {
@@ -153,10 +219,9 @@ int main(int argc, char* argv[], char* envp[]) {
         read_source(argv[1]);
     } else if (argc > 1 && argv[1][0] == '-') {
         if (strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "--dev") == 0) {
-            std::cout << "Development mode enabled!" << std::endl;
             dev_mode= true;
             debug_info.open("./swy.log");
-            debug_info << "--- start of log ---" << std::endl;
+            debug_info << "--- start of log ---\n" << std::endl;
         }
         if (argc >= 3)
             read_source(argv[2]); // FIXME: Why does this crash?
@@ -164,6 +229,9 @@ int main(int argc, char* argv[], char* envp[]) {
             read_source_from_stream(std::cin);
     } else
         read_source(argv[1]);
+
+    if (dev_mode)
+        debug_info << "\n--- start of execution ---\n" << std::endl;
 
     char current_command= '9'; // Halt if nothing is given
     if (linelen == 0 || playfield.size() == 0)
@@ -173,10 +241,125 @@ int main(int argc, char* argv[], char* envp[]) {
         pos[1]         = pos[1] % linelen;
         current_command= playfield[pos[0]][pos[1]];
         if (dev_mode)
-            debug_info << "Moving " << direction_names[direction % 4] << " to "
-                       << get_command_name(current_command) << '[' << current_command
-                       << ']' << " at l" << pos[0] << 'c' << pos[1] << std::endl;
+            debug_info << "Moved " << direction_names[direction % 4] << " to "
+                       << get_command_name(current_command) << " at l:0o" << std::oct
+                       << pos[0] << ",c:0o" << pos[1] << std::dec << std::endl;
         switch (current_command) {
+            case '/': {
+                uint64_t values[2]= { pop_stack(), pop_stack() };
+                if (values[0] > values[1]) {
+                    if (direction == RIGHT || direction == LEFT)
+                        direction= Direction(direction + 1);
+                    else
+                        direction= Direction(direction - 1);
+                } else {
+                    // Ignore this potential turn
+                }
+                break;
+            }
+            case '\\': {
+                uint64_t values[2]= { pop_stack(), pop_stack() };
+                if (values[0] > values[1]) {
+                    if (direction == RIGHT || direction == LEFT)
+                        direction= Direction(direction - 1);
+                    else
+                        direction= Direction(direction + 1);
+                } else {
+                    // Ignore this potential turn
+                }
+                break;
+            }
+            case '?': {
+                get_input();
+                break;
+            }
+            case '!': {
+                send_output();
+                break;
+            }
+            case '%': {
+                pos[0]= pop_stack();
+                pos[1]= pop_stack();
+                break;
+            }
+            case '0': {
+                push_stack(0);
+                break;
+            }
+            case '1': {
+                push_stack(1);
+                break;
+            }
+            case '2': {
+                push_stack(2);
+                break;
+            }
+            case '3': {
+                push_stack(3);
+                break;
+            }
+            case '4': {
+                push_stack(4);
+                break;
+            }
+            case '5': {
+                push_stack(5);
+                break;
+            }
+            case '6': {
+                push_stack(6);
+                break;
+            }
+            case '7': {
+                push_stack(7);
+                break;
+            }
+            case '+': {
+                push_stack(pop_stack() + pop_stack());
+                break;
+            }
+            case '_': {
+                push_stack(~pop_stack());
+                break;
+            }
+            case '#': {
+                if (stack.size() <= 1)
+                    break;
+                uint64_t raw_i= pop_stack();
+                uint64_t i= (stack.size() - i) % stack.size(); // Choose a position inside the stack
+                uint64_t value_i= stack[i];
+                stack[i]        = pop_stack();
+                push_stack(value_i);
+                break;
+            }
+            case '@': {
+                if (stack.size() <= 1) {
+                    if (dev_mode)
+                        debug_info << "Stack is too short, cannot swap values!"
+                                   << std::endl;
+                    break;
+                }
+                uint64_t raw_i= pop_stack();
+                uint64_t i= (stack.size() - i) % stack.size(); // Choose a position inside the stack
+                push_stack(stack[i]);
+                if (dev_mode)
+                    debug_info << "Pushed copy of 0o" << std::oct << i
+                               << "th stack element (value 0o" << stack.back()
+                               << ") to stack" << std::dec << std::endl;
+                break;
+            }
+            case '8': {
+                if (dev_mode)
+                    debug_info << "Pushing coordinates to stack..." << std::endl;
+                push_stack(pos[0]);
+                push_stack(pos[1]);
+                break;
+            }
+            case '9': {
+                if (dev_mode)
+                    debug_info << "Halting!" << std::endl;
+                return 0;
+            }
             default: {
                 // Ignore unrecognized character
             }
