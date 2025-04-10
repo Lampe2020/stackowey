a2cc46f9a114bd3fa8348a29be2c98b23cad03d2
Lampe2020 <kontakt@lampe2020.de>
2025-04-10 10:11:01 +0200

    Make `compile.sh` output a bit nicer

--- compile.sh
+++ compile.sh
@@ -9,12 +9,12 @@ then
     wine C:\\Dev-Cpp\\bin\\g++.exe -std=c++23 -finput-charset=UTF-8 -fexec-charset=UTF-8 -pipe -o stackowey.exe --debug main.cpp &&
     printf "Running...\n" &&
     wineconsole stackowey.exe -d
-    printf 'Ended with exit code %s\n' "$?"
+    printf '\n-> Ended with exit code %s\n' "$?"
 else
     rm -v stackowey
     printf "Compiling...\n"
     g++ -std=c++23 -o stackowey --debug -pipe main.cpp &&
     printf "Running...\n" &&
     ./stackowey -d
-    printf 'Ended with exit code %s\n' "$?"
+    printf '\n-> Ended with exit code %s\n' "$?"
 fi
