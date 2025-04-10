3a13433cac5539d8f1093d1ea23870187ecfe413
Lampe2020 <kontakt@lampe2020.de>
2025-04-10 16:45:30 +0200

    Remove option to compile in WINE
    → that did no longer work with the code in this repo and I'm too lazy
      to fix that. So to run Stackowey on Window$, compile it inside Cygwin
      or something similar.

--- compile.sh
+++ compile.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-if [ "$1" = 'w' ]
-then
-    export WINEDEBUG=-all
-    rm -v stackowey.exe
-    printf "Compiling...\n"
-    #TODO: Reencode source to windows-1258 encoding first? 
-    wine C:\\Dev-Cpp\\bin\\g++.exe -std=c++23 -finput-charset=UTF-8 -fexec-charset=UTF-8 -pipe -o stackowey.exe --debug main.cpp &&
-    printf "Running...\n" &&
-    wineconsole stackowey.exe -d
-    printf '\n-> Ended with exit code %s\n' "$?"
-else
-    rm -v stackowey
-    printf "Compiling...\n"
-    g++ -std=c++23 -o stackowey --debug -pipe main.cpp &&
-    printf "Running...\n" &&
-    ./stackowey -d
-    printf '\n-> Ended with exit code %s\n' "$?"
-fi
+rm -v stackowey
+printf "Compiling...\n"
+g++ -std=c++23 -o stackowey --debug -pipe main.cpp &&
+printf "Running...\n" &&
+./stackowey -d
+printf '\n-> Ended with exit code %s\n' "$?"
