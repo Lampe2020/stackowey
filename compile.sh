d3799ee140b46e2af11f2c1f45f9b697f16cdb06
Lampe2020 <kontakt@lampe2020.de>
2025-07-07 06:55:33 +0200

    Make debug mode optional in compile script
    → Note: for some reason the interactive mode start message gets suppressed in debug mode? No clue why...

--- compile.sh
+++ compile.sh
@@ -2,7 +2,14 @@
 # -*- coding: utf-8 -*-
 rm -v stackowey
 printf "Compiling...\n"
-g++ -Wall -Werror -O3 -ffast-math -std=c++23 -o stackowey --debug -pipe main.cpp &&
-printf "Running...\n" &&
-./stackowey -d
+if [ "$1" = "-d" ]
+then
+    g++ --debug -std=c++23 -o stackowey -pipe main.cpp &&
+    printf "Running...\n" &&
+    ./stackowey -d
+else
+    g++ -Wall -Werror -O3 -ffast-math -std=c++23 -o stackowey -pipe main.cpp &&
+    printf "Running...\n" &&
+    ./stackowey
+fi
 printf '\n-> Ended with exit code %s\n' "$?"
