24dd556382b88deb3ee942a27d44388895d81d84
Lampe2020 <kontakt@lampe2020.de>
2025-07-07 06:32:24 +0200

    Optimize the compiled binaries slightly
    * Enable optimizations
    * Enable all warnings
    * Treat all warnings as errors

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 rm -v stackowey
 printf "Compiling...\n"
-g++ -std=c++23 -o stackowey --debug -pipe main.cpp &&
+g++ -Wall -Werror -O3 -ffast-math -std=c++23 -o stackowey --debug -pipe main.cpp &&
 printf "Running...\n" &&
 ./stackowey -d
 printf '\n-> Ended with exit code %s\n' "$?"
